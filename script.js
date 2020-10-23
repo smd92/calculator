9a2d78a2a328348b78c396632b7334461ef26367
sassan <sassan.marouf@gmail.com>
2020-10-23 16:29:09 +0200

    .

--- script.js
+++ script.js
@@ -23,30 +23,6 @@ function power(inputOne, inputTwo) {
 	return inputOne ** inputTwo;
 }
 
-/*
-function factorial(num) {
-	if (num === 0 || num === 1) {
-		return 1;
-	}
-
-	else {
-		for (let i = num - 1; i >= 1; i--) {
-			num = num * i;
-		}
-	}
-	return num;
-}
-
-function sum(input) {
-    if (input.length > 0) {
-      return input.reduce((a, b) => a + b);
-    }
-    else {
-      return 0;
-    }
-}
-*/
-
 function operate(operator, numOne, numTwo) {
 	if (operator === "+") {
 		add(numOne, numTwo);
@@ -60,4 +36,14 @@ function operate(operator, numOne, numTwo) {
 	else if (operator === "/") {
 		divide(numOne, numTwo);
 	}
+}
+
+const clear = document.querySelector("#clear");
+const displayContent = document.querySelector("#displayContent");
+const numbers = document.querySelectorAll(".number");
+
+for (let i = 0; i < numbers.length; i++) {
+	numbers[i].addEventListener("click", () => {
+		displayContent.innerHTML += numbers[i].textContent;
+	})
 }
